a9e8b629fd764e4963ad759204e4aa47d6e7cb18
Matheus Alexandre <alexandremhm@gmail.com>
2022-01-03 15:28:13 -0300

    initial commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <span className="logo">TRYBE</span>
       <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass D
+        Glass
       </object>
     </div>
   );
