fdba3ea20739ba987903631a124aae14bcc048b6
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2022-01-03 21:50:41 -0300

    bugfix-ESlint-fixed

--- back-end/src/middlewares/Validations.js
+++ back-end/src/middlewares/Validations.js
@@ -1,11 +1,11 @@
-const { Users } = require('../database/models');
 const { Op } = require('sequelize');
+const { Users } = require('../database/models');
 // const helpers = require('../helpers');
 
 const registerValidations = async (req, res, next) => {
   const { name, email } = req.body;
 
-  const doesUserExist = await Users.findOne({ where: { [Op.or]: [{name}, {email}] } });
+  const doesUserExist = await Users.findOne({ where: { [Op.or]: [{ name }, { email }] } });
   if (doesUserExist) return res.status(409).json('Conflict');
 
   next();
