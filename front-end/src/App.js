5bec9ab3f1baf454f64b922e4943f5659f951ae4
Ghislaine <glenox79@gmail.com>
2022-01-04 07:46:30 -0300

    feature-login-screen-and-validations

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Redirect, Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass D
-      </object>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+        <Route exact path="/login" component={ Login } />
+      </Switch>
+    </Router>
   );
 }
 
