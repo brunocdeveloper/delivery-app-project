826ac82c9b2e542162051ec7b88a995e4d05fb1e
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2022-01-03 21:45:55 -0300

    feature-refactor-findOne-function

--- back-end/src/middlewares/Validations.js
+++ back-end/src/middlewares/Validations.js
@@ -1,10 +1,11 @@
 const { Users } = require('../database/models');
+const { Op } = require('sequelize');
 // const helpers = require('../helpers');
 
 const registerValidations = async (req, res, next) => {
   const { name, email } = req.body;
 
-  const doesUserExist = await Users.findOne({ name, email });
+  const doesUserExist = await Users.findOne({ where: { [Op.or]: [{name}, {email}] } });
   if (doesUserExist) return res.status(409).json('Conflict');
 
   next();
