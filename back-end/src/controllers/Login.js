3d46826230b1d1fbddcaf7fbf70145d3efbfa09e
Bruno <brunocmoraes97@gmail.com>
2022-01-04 17:15:38 -0300

    Feature req5 - lint

--- back-end/src/controllers/Login.js
+++ back-end/src/controllers/Login.js
@@ -1,5 +1,5 @@
-const Service = require('../services/Login');
 const md5 = require('md5');
+const Service = require('../services/Login');
 
 const verifyExistenceUser = async (req, res, next) => {
   const { email } = req.body;
