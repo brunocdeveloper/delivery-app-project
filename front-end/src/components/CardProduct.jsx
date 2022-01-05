ed1ff9fdcf85a628da96d72c607173c2be9ce47d
Ghislaine <glenox79@gmail.com>
2022-01-05 16:01:43 -0300

    feature-datatestid-cardproducts

--- front-end/src/components/CardProduct.jsx
+++ front-end/src/components/CardProduct.jsx
@@ -2,29 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function CardProduct(props) {
-  const { product: { name, price, url_image: urlImage } } = props;
+  const { product: { id, name, price, url_image: urlImage } } = props;
 
   return (
-    <>
+    <main id={ id }>
       <section>
-        <span>{ price }</span>
+        <span
+          data-testid={ `customer_products__element-card-price-${id}` }
+        >
+          { price }
+        </span>
         <img
           src={ urlImage }
           alt={ name }
+          data-testid={ `customer_products__img-card-bg-image-${id}` }
         />
       </section>
       <footer>
-        <span>{ name }</span>
-        <button type="button">-</button>
-        <span>0</span>
-        <button type="button">+</button>
+        <span
+          data-testid={ `customer_products__element-card-title-${id}` }
+        >
+          { name }
+        </span>
+        <button
+          type="button"
+          data-testid={ `customer_products__button-card-add-item-${id}` }
+        >
+          -
+        </button>
+        <span
+          data-testid={ `customer_products__input-card-quantity-${id}` }
+        >0</span>
+        <button
+          type="button"
+          data-testid={ `customer_products__button-card-add-item-${id}` }
+        >
+          +
+        </button>
       </footer>
-    </>
+    </main>
   );
 }
 
 CardProduct.propTypes = {
   product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
     url_image: PropTypes.string.isRequired,
