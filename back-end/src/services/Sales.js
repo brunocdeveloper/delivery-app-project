00cabc9297a3f38365a818387e4bb7eba8662973
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2022-01-06 16:53:16 -0300

    FEATURE-Finished-Route-customers/checkout-Backend

--- back-end/src/services/Sales.js
+++ back-end/src/services/Sales.js
@@ -1,16 +1,5 @@
 const { sales } = require('../database/models');
 
-const moment = require('moment');
-const localDate = new Date();
-console.log(moment.utc(localDate).format());
-
-// const {
-//   total_price,
-//   delivery_address,
-//   delivery_number,
-//   seller_id,
-// } = sale;
-
 const createSale = async (sale, id) => {
   const createdSale = await sales.create({ ...sale, user_id: id });
   if (!createdSale) throw new Error('Server unavailable. Please try again later.');
