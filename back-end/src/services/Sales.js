1b25a233c8c3224779e998a90025f5df3b119462
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2022-01-06 22:20:29 -0300

    refactoring-removing-comments

--- back-end/src/services/Sales.js
+++ back-end/src/services/Sales.js
@@ -1,14 +1,6 @@
 const { sales, sales_products: salesProducts } = require('../database/models');
 const { organizeProdArray } = require('../helpers');
 
-// sale = {
-//   seller_id,
-//   delivery_number,
-//   delivery_address,
-//   total_price,
-//   prodArray,
-// }
-
 const createSale = async (saleObj, id) => {
   const { prodArray, ...newObj } = saleObj;
   const createdSale = await sales.create({ ...newObj, user_id: id });
