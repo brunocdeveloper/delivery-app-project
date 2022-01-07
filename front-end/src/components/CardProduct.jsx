8885713beca1f7e2d2cd516bb0f7dfeaef51340d
Bruno <brunocmoraes97@gmail.com>
2022-01-07 15:50:03 -0300

    Requisito 15

--- front-end/src/components/CardProduct.jsx
+++ front-end/src/components/CardProduct.jsx
@@ -1,10 +1,92 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import AppContext from '../context/AppContext';
 
 export default function CardProduct(props) {
   const { product: { id, name, price, url_image: urlImage } } = props;
-
+  const [quantity, setQuantity] = useState(0);
+  const { cartItens, setCartItens, setSubTotal, subTotal } = useContext(AppContext);
   const changePriceToComa = (value) => value.toString().replace(/\./, ',');
+
+  const removeItem = () => {
+    const foundIndexItem = cartItens.findIndex((item) => item.id === id);
+    const negativeOne = -1;
+    if (foundIndexItem === negativeOne) {
+      return setCartItens([...cartItens, { id, name, price, quantity: quantity + 1 }]);
+    }
+
+    const newCartItem = cartItens;
+    newCartItem[foundIndexItem].quantity -= 1;
+    if (newCartItem[foundIndexItem].quantity === 0) {
+      newCartItem.splice(foundIndexItem, 1);
+      return setCartItens(newCartItem);
+    }
+    setCartItens(newCartItem);
+  };
+
+  const addItem = () => {
+    const foundIndexItem = cartItens.findIndex((item) => item.id === id);
+    const negativeOne = -1;
+    if (foundIndexItem === negativeOne) {
+      return setCartItens([...cartItens, { id, name, price, quantity: quantity + 1 }]);
+    }
+
+    const newCartItem = cartItens;
+    newCartItem[foundIndexItem].quantity += 1;
+    setCartItens(newCartItem);
+  };
+
+  const addByInput = () => {
+    if (cartItens.length === 0) return;
+    const foundIndexItem = cartItens.findIndex((item) => item.id === id);
+    const negativeOne = -1;
+    if (foundIndexItem === negativeOne) {
+      return setCartItens([...cartItens, { id, name, price, quantity }]);
+    }
+
+    const newCartItem = cartItens;
+    newCartItem[foundIndexItem].quantity = quantity;
+    setCartItens(newCartItem);
+  };
+
+  const calculateTotal = () => {
+    const totalPrice = cartItens
+      .reduce((acc, { price: unitPrice, quantity: qty }) => acc + (unitPrice * qty), 0);
+    setSubTotal(totalPrice);
+  };
+
+  useEffect(() => {
+    calculateTotal();
+    addByInput();
+    console.log(cartItens);
+  }, [quantity, subTotal, calculateTotal, addByInput]);
+
+  const addOrRemoveQuantity = ({ target }) => {
+    const parseQuantity = Number(target.value);
+    switch (target.name) {
+    case 'remove':
+      if (quantity < 1) {
+        setQuantity(0);
+      } else {
+        setQuantity(quantity - 1);
+      }
+      if (quantity > 0) {
+        removeItem();
+      }
+      break;
+    case 'add':
+      setQuantity(quantity + 1);
+      addItem();
+      break;
+    case 'input':
+      setQuantity(parseQuantity);
+      addItem();
+      calculateTotal();
+      break;
+    default:
+      break;
+    }
+  };
 
   return (
     <main id={ id }>
@@ -15,6 +97,7 @@ export default function CardProduct(props) {
           { changePriceToComa(price) }
         </span>
         <img
+          width="60px"
           src={ urlImage }
           alt={ `${name}` }
           data-testid={ `customer_products__img-card-bg-image-${id}` }
@@ -27,19 +110,25 @@ export default function CardProduct(props) {
           { name }
         </span>
         <button
+          idCart={ id }
+          name="remove"
           type="button"
-          data-testid={ `customer_products__button-card-add-item-${id}` }
+          data-testid={ `customer_products__button-card-rm-item-${id}` }
+          onClick={ ({ target }) => addOrRemoveQuantity({ target }) }
         >
           -
         </button>
-        <span
+        <input
+          name="input"
           data-testid={ `customer_products__input-card-quantity-${id}` }
-        >
-          0
-        </span>
+          onChange={ (e) => addOrRemoveQuantity(e) }
+          value={ quantity }
+        />
         <button
+          name="add"
           type="button"
           data-testid={ `customer_products__button-card-add-item-${id}` }
+          onClick={ ({ target }) => addOrRemoveQuantity({ target }) }
         >
           +
         </button>
@@ -52,7 +141,8 @@ CardProduct.propTypes = {
   product: PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
+    price: PropTypes.string.isRequired,
     url_image: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
   }).isRequired,
 };
