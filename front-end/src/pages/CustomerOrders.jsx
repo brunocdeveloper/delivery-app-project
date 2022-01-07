84b18017aab1910b6129f18c143ab4d77451a656
Ghislaine <glenox79@gmail.com>
2022-01-07 17:34:24 -0300

    fix-useEffect

--- front-end/src/pages/CustomerOrders.jsx
+++ front-end/src/pages/CustomerOrders.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppContext from '../context/AppContext';
 import NavBar from '../components/NavBar';
 import CardOrder from '../components/CardOrder';
