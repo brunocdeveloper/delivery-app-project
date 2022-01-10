ff3a6d93757b34fa139f76240482c88e891790f4
Ghislaine <glenox79@gmail.com>
2022-01-10 18:38:44 -0300

    feature-admin-validations

--- front-end/src/components/AdminForm.jsx
+++ front-end/src/components/AdminForm.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function AdminForm() {
+  const [
+    admName,
+    admSetName,
+    email,
+    setEmail,
+    password,
+    setPassword,
+  ] = useState('');
+
+  const validateLogin = () => {
+    const nameLenght = 12;
+    const pwdLength = 6;
+    const validRegex = new RegExp(
+      /^([\w.-]+)@([\w-]+)((\.(\w){2,3})+)$/,
+    ); 
+    const validEmail = validRegex.test(email);
+    if (validEmail
+      && password.length >= pwdLength
+      && admName.length >= nameLenght) return true;
+    return false;
+  };
+
   return (
     <section>
       <form>
@@ -8,27 +30,33 @@ export default function AdminForm() {
           Nome
           <input
             type="text"
+            name="name"
             id="name"
             placeholder="Nome e Sobrenome"
             data-testid="admin_manage__input-name"
+            onChange={ (e) => admSetName(e.target.value) }
           />
         </label>
         <label htmlFor="email">
           Email
           <input
             type="text"
+            name="email"
             id="email"
             placeholder="seu-email@site.com.br"
             data-testid="admin_manage__input-email"
+            onChange={ (e) => setEmail(e.target.value) }
           />
         </label>
         <label htmlFor="password">
           Senha
           <input
             type="text"
+            name="password"
             id="password"
             placeholder="******"
             data-testid="admin_manage__input-password"
+            onChange={ (e) => setPassword(e.target.value) }
           />
         </label>
         <label htmlFor="type">
@@ -47,6 +75,7 @@ export default function AdminForm() {
         <button
           type="button"
           data-testid="admin_manage__button-register"
+          disabled={ !validateLogin() }
         >
           CADASTRAR
         </button>
