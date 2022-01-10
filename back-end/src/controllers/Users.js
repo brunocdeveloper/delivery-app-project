47b6cc29059a2cd3edd903ab6683903c0f27f2fa
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2022-01-10 18:38:32 -0300

    FEATURE-FINISHED-orderDetails-routes-dynamically

--- back-end/src/controllers/Users.js
+++ back-end/src/controllers/Users.js
@@ -11,6 +11,18 @@ const getUsersByRole = async (req, res) => {
   }
 };
 
+const getUserOrders = async (req, res) => {
+  try {
+    const { data: { id, role } } = req.userInfo;
+    const foundOrders = await Service.getUserOrders(id, role);
+    return res.status(200).json(foundOrders);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ message: e.message });
+  }
+};
+
 module.exports = {
   getUsersByRole,
+  getUserOrders,
 };
