cd04659e890020894296d40227d7dfb2044c1d62
Ghislaine <glenox79@gmail.com>
2022-01-11 14:59:45 -0300

    feature-customer-orders

--- front-end/src/pages/CustomerOrders.jsx
+++ front-end/src/pages/CustomerOrders.jsx
@@ -1,33 +1,11 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import AppContext from '../context/AppContext';
 import NavBar from '../components/NavBar';
 import CustomerCardOrder from '../components/CustomerCardOrder';
+import getUserOrders from '../api/getOrders';
 
 export default function CustomerOrders() {
-  const arrOrders = [
-    {
-      id: 1,
-      Pedido: '001',
-      status: 'PENDENTE',
-      saleDate: '08/04/21',
-      totalPrice: 'R$ 23,80',
-    },
-    {
-      id: 2,
-      Pedido: '002',
-      status: 'PREPARANDO',
-      saleDate: '08/04/21',
-      totalPrice: 'R$ 14,20',
-    },
-    {
-      id: 3,
-      Pedido: '003',
-      status: 'ENTREGUE',
-      saleDate: '07/04/21',
-      totalPrice: 'R$ 28,46',
-    },
-  ];
-
-  // const { orders, setOrders } = useContext(AppContext);
+  const { orders, setOrders } = useContext(AppContext);
 
   const section1 = {
     name: 'PRODUTOS',
@@ -37,20 +15,22 @@ export default function CustomerOrders() {
     name: 'MEUS PEDIDOS',
   };
 
-  /* const getOrders = async () => {
+  const getOrders = async () => {
     const user = JSON.parse(localStorage.getItem('user'));
-    const result = await getUserOrders(user.id);
+    console.log(user, 'USER');
+    const result = await getUserOrders('customer', user.token);
+    console.log(result, 'RESULT');
     setOrders(result);
   };
 
   useEffect(() => {
     getOrders();
-  }, []); */
+  }, []);
 
   return (
     <section>
       <NavBar section1={ section1 } section2={ section2 } />
-      { arrOrders
+      { orders && orders
         .map((order, index) => <CustomerCardOrder key={ index } order={ order } />) }
     </section>
   );
