2ccf1f7796f09ebb67c6beac1170f42119667dc3
Matheus Alexandre <alexandremhm@gmail.com>
2022-01-12 16:57:14 -0300

    feature-order-status-routes

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,10 +2,12 @@ const login = require('./login');
 const register = require('./register');
 const customer = require('./customer');
 const seller = require('./seller');
+const order = require('./order');
 
 module.exports = {
   login,
   register,
   customer,
   seller,
+  order,
 };
