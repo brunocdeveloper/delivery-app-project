8595c4356b6f7e96154b79ab0f02147259475d26
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2022-01-13 18:15:32 -0300

    added-title-adminTable

--- front-end/src/components/AdminTable.jsx
+++ front-end/src/components/AdminTable.jsx
@@ -14,34 +14,37 @@ export default function AdminTable() {
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Item</th>
-          <th>Nome</th>
-          <th>E-mail</th>
-          <th>Tipo</th>
-          <th>Excluir</th>
-        </tr>
-      </thead>
-      <tbody>
-        { usersList.length > 0 && usersList.map((user, i) => (
-          <tr key={ user.name }>
-            <td data-testid={ `${dataidCommon}-item-number-${i}` }>{i + 1}</td>
-            <td data-testid={ `${dataidCommon}-name-${i}` }>{user.name}</td>
-            <td data-testid={ `${dataidCommon}-email-${i}` }>{user.email}</td>
-            <td data-testid={ `${dataidCommon}-role-${i}` }>{user.role}</td>
-            <button
-              name={ user.id }
-              data-testid={ `${dataidCommon}-remove-${i}` }
-              type="button"
-              onClick={ ({ target }) => deleteUser(target.name) }
-            >
-              Excluir
-            </button>
+    <>
+      <h3>Lista de usuários</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Tipo</th>
+            <th>Excluir</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          { usersList.length > 0 && usersList.map((user, i) => (
+            <tr key={ user.name }>
+              <td data-testid={ `${dataidCommon}-item-number-${i}` }>{i + 1}</td>
+              <td data-testid={ `${dataidCommon}-name-${i}` }>{user.name}</td>
+              <td data-testid={ `${dataidCommon}-email-${i}` }>{user.email}</td>
+              <td data-testid={ `${dataidCommon}-role-${i}` }>{user.role}</td>
+              <button
+                name={ user.id }
+                data-testid={ `${dataidCommon}-remove-${i}` }
+                type="button"
+                onClick={ ({ target }) => deleteUser(target.name) }
+              >
+                Excluir
+              </button>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
