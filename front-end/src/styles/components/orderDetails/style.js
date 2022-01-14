29e78cbca3d2f8f89a7a4adcd020fecaaa64d118
Bruno <brunocmoraes97@gmail.com>
2022-01-14 16:40:53 -0300

    Att local

---
+++ front-end/src/styles/components/orderDetails/style.js
@@ -0,0 +1,135 @@
+import styled from 'styled-components';
+
+export const ContainerOrderDetails = styled.div`
+  background-color: green;
+  width: 1640px;
+  margin: 200px auto;
+`;
+
+export const ContainerInformation = styled.div`
+  height: 70px;
+  background-color: pink;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #EAF1EF;
+`;
+
+export const ContainerOrdersItems = styled.div`
+/*   width: 1634px;
+  height: 727px;
+  top: 80px;
+  left: 1px;
+  position: absolute;
+  left: 0.06%;
+  right: 0.06%;
+  bottom: 0.11%;
+
+  background: #FBFFFE; */
+`;
+
+export const TitleDetail = styled.span`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 36px;
+  line-height: 42px;
+  display: inline-block;
+  margin-bottom: 20px;
+`;
+
+export const ButtonDelivered = styled.button`
+  margin-right: 20px;
+  height: 52px;
+  background: #036B52;
+  border-radius: 10px;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
+  color: #F2FFFC; 
+  border-style: none;
+`;
+
+export const OrderStatus = styled.span`
+  display: inline-block;
+  width: 230px;
+  height: 52px;
+  text-align: center;
+  margin-right: 15px;
+
+  background: rgba(0, 204, 155, 0.75);
+  border-radius: 10px;
+
+  border-style: none;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 42px;
+  text-align: center;
+  color: #001813;
+`;
+
+export const OrderId = styled.span`
+  margin-left: 20px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 42px;
+  text-indent: 20px;
+
+  color: #001813; 
+`;
+
+export const Seller = styled.span`
+  margin-left: 10px;
+  font-style: normal;
+  font-size: 36px;
+  line-height: 42px;
+  align-items: center;
+  text-indent: 20px; 
+`;
+
+export const SellerName = styled.span`
+  margin-left: 10px;
+  font-style: normal;
+  font-size: 36px;
+  line-height: 42px;
+  align-items: center;
+  text-indent: 20px;
+`;
+
+export const SaleDate = styled.span`
+  display: inline-block;
+  text-align: center;
+  width: 300px;
+  margin-left: 25px;
+  background: rgba(242, 255, 252, 0.75);
+  border-radius: 10px;
+
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 42px;
+  color: #001813;
+`;
+
+export const TotalPrice = styled.span`
+ /*  position: absolute;
+  width: 300px;
+  left: 81%;
+  top: 84.91%;
+  bottom: 6.89%;
+
+  background: #036B52;
+  border-radius: 10px;
+
+  font-style: normal;
+  font-weight: bold;
+  font-size: 64px;
+  line-height: 75px;
+
+  text-align: center;
+
+  color: #F2FFFC; */
+`;
